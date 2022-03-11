4f4744ce447f96ef83508f2d07f885b8a7590635
baegteun <baegteun@gmail.com>
2022-03-11 14:49:26 +0900

    Initial Commit

---
+++ numbering/numberingApp.swift
@@ -0,0 +1,17 @@
+//
+//  numberingApp.swift
+//  numbering
+//
+//  Created by 최형우 on 2022/03/11.
+//
+
+import SwiftUI
+
+@main
+struct numberingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
